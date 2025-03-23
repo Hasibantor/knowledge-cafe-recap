bbb544b371e46d8e6c2180b9a82818ed6fc8478b
Hasibantor <hasib54069@gmail.com>
2025-03-23 17:13:08 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",   // আপনার HTML ফাইল যেখানে Tailwind CSS প্রয়োগ হবে
+      "./src/**/*.{js,jsx,ts,tsx}",  // আপনার React কম্পোনেন্ট ফাইলগুলো
+    ],
+    theme: {
+      extend: {
+       
+        },
+      },
+    
+    plugins: [],
+  }
+  
